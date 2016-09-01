2199ed34aed199beefb0d6b5755dc2db97f72d7a
Daniel Li <neolee11@yahoo.com>
2016-09-01 16:40:01 -0400

    progress

---
+++ market/test/market_spec.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var expect = require('chai').expect;
+
+var market = require('../index');
+
+describe("market tests", function () {
+   describe("functionality tests", function () {
+       it("it should return $300", function () {
+          var result = market.getMoney("dollar");
+          expect(result).include('300');
+       });
+   })
+});
